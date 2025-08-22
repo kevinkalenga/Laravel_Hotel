63d9e17d82783bf1b4aac98eebf01864ce1be7b9
Kalenga243 <kevinkalenga10@gmail.com>
2025-08-23 01:54:11 +0200

    Admin Template Setup

--- Hotel/bootstrap/app.php
+++ Hotel/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-              'adminrole' => \App\Http\Middleware\AdminRole::class,
+              'role' => \App\Http\Middleware\AdminRole::class,
             ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
