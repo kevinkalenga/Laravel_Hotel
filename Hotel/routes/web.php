5d504c9bb3c8622d2f8f75bbb26baf4fc0c6e209
Kalenga243 <kevinkalenga10@gmail.com>
2025-08-23 03:48:21 +0200

    Customize login form

--- Hotel/routes/web.php
+++ Hotel/routes/web.php
@@ -25,6 +25,8 @@ Route::middleware(['auth',  'role:admin'])->group(function() {
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
+ Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
 
 
 
